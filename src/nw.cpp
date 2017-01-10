5b9a197f53985053cc1c5ebda3e32f6d2d9aad5b
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-01-10 15:21:45 +0100

    Format

--- src/nw.cpp
+++ src/nw.cpp
@@ -1,24 +1,28 @@
-#include "mpi.h"
-
 #include "nw.hpp"
 
+#include "mpi.h"
 
-void send_uns(unsigned val, unsigned destination, unsigned tag) {
+
+void send_uns(unsigned val, unsigned destination, unsigned tag)
+{
     MPI_Send(&val, 1, MPI_UNSIGNED, destination, tag, MPI_COMM_WORLD);
 }
 
-unsigned recv_uns(unsigned source, unsigned tag) {
+unsigned recv_uns(unsigned source, unsigned tag)
+{
     unsigned val;
     MPI_Recv(&val, 1, MPI_UNSIGNED, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     return val;
 }
 
 
-void send_vec(const arma::vec& vec, unsigned destination, unsigned tag) {
+void send_vec(const arma::vec& vec, unsigned destination, unsigned tag)
+{
     MPI_Send(vec.cbegin(), vec.size(), MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
 }
 
-arma::vec recv_vec(unsigned source, unsigned tag) {
+arma::vec recv_vec(unsigned source, unsigned tag)
+{
     MPI_Status status;
     MPI_Probe(source, tag, MPI_COMM_WORLD, &status);
 
@@ -31,11 +35,13 @@ arma::vec recv_vec(unsigned source, unsigned tag) {
 }
 
 
-void send_mat(const arma::mat& mat, unsigned destination, unsigned tag) {
+void send_mat(const arma::mat& mat, unsigned destination, unsigned tag)
+{
     MPI_Send(mat.cbegin(), mat.size(), MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
 }
 
-arma::mat recv_mat(unsigned source, unsigned tag, unsigned n_rows) {
+arma::mat recv_mat(unsigned source, unsigned tag, unsigned n_rows)
+{
     MPI_Status status;
     MPI_Probe(source, tag, MPI_COMM_WORLD, &status);
 
