5757fa840365b46bf3a0abf71e0821fc4b3bf1e0
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-01-15 10:20:56 +0100

    Add CSC and CSR to utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "armadillo"
 
@@ -12,12 +13,38 @@
 struct Graph {
     uint_fast32_t num_nodes;
     std::map<uint_fast32_t, std::set<uint_fast32_t>> edges;
+    std::map<uint_fast32_t, std::set<uint_fast32_t>> inv_edges;
 
     Graph(const std::string&);
 };
 
 
 arma::sp_fmat to_adjacency_mat(const Graph&);
+
+
+struct CSC {
+    size_t num_rows, num_cols;
+    std::vector<float> a;
+    std::vector<uint_fast32_t> ia;
+    std::vector<uint_fast32_t> ja;
+
+    CSC() {}
+    CSC(const Graph&);
+};
+
+struct CSR {
+    size_t num_rows, num_cols;
+    std::vector<float> a;
+    std::vector<uint_fast32_t> ia;
+    std::vector<uint_fast32_t> ja;
+
+    CSR() {}
+    CSR(const Graph&);
+
+    arma::fvec operator*(const arma::fvec&) const;
+};
+
+CSR transpose(const CSC&);
 
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cassert>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
@@ -29,8 +30,12 @@ Graph::Graph(const std::string& filename)
         }
 
         edges[from_node].insert(to_node);
-        num_nodes = 1 + std::max(std::max(from_node, to_node), num_nodes);
+        inv_edges[to_node].insert(from_node);
+
+        num_nodes = std::max(std::max(from_node, to_node), num_nodes);
     }
+    // assume the node ids start from zero
+    ++num_nodes;
 
     file.close();
 }
@@ -55,4 +60,76 @@ arma::sp_fmat to_adjacency_mat(const Graph& graph)
         }
     }
     return adjacency_mat;
+}
+
+
+CSC::CSC(const Graph& graph)
+{
+    num_rows = num_cols = graph.num_nodes;
+
+    // find nodes without outgoing edges
+    std::set<uint_fast32_t> dangling_nodes;
+    for (uint_fast32_t node = 0; node < graph.num_nodes; ++node) {
+        if (graph.edges.count(node) == 0) {
+            dangling_nodes.insert(node);
+        }
+    }
+
+    uint_fast32_t num_values = 0;
+    ia.push_back(num_values);
+
+    for (uint_fast32_t to_node = 0; to_node < graph.num_nodes; ++to_node) {
+        const auto& in_nodes = graph.inv_edges.count(to_node) > 0 ?
+                               graph.inv_edges.at(to_node) :
+                               std::set<uint_fast32_t>();
+
+        // find which rows have a value (different from zero)
+        auto from_nodes = in_nodes;
+        from_nodes.insert(dangling_nodes.begin(), dangling_nodes.end());
+
+        // store the current column in csc
+        for (auto from_node: from_nodes) {
+            const float weight = dangling_nodes.count(from_node) > 0 ?
+                                 1.0f/graph.num_nodes :
+                                 1.0f/in_nodes.size();
+            a.push_back(weight);
+            ja.push_back(from_node);
+        }
+        num_values += from_nodes.size();
+        ia.push_back(num_values);
+    }
+}
+
+
+arma::fvec CSR::operator*(const arma::fvec& vec) const
+{
+    // see http://www.mathcs.emory.edu/~cheung/Courses/561/Syllabus/3-C/sparse.html
+    arma::fvec x = arma::zeros<arma::fvec>(num_rows);
+    for (std::size_t i = 0; i < num_rows; ++i) {
+        if (ia[i] == ia[i+1]) {
+            // TO CHECK
+            // std::cout << "SUCA" << std::endl;
+            // for (std::size_t k = 0; k < num_rows; ++k) {
+            //     x[i] += (1.0f/num_rows) * vec[k];
+            // }
+        }
+        else {
+            for (std::size_t k = ia[i]; k < ia[i+1]; ++k) {
+                x[i] = x[i] + a[k] * vec[ja[k]];
+            }
+        }
+    }
+    return x;
+}
+
+
+CSR transpose(const CSC& csc)
+{
+    CSR csr;
+    csr.num_rows = csc.num_cols;
+    csr.num_cols = csc.num_rows;
+    csr.a = csc.a;
+    csr.ia = csc.ia;
+    csr.ja = csc.ja;
+    return csr;
 }
