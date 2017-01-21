04965519d7a91a52d9d9a818c8c8a4bf5cba9465
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-01-21 10:43:54 +0100

    Format

--- examples/mpi-pagerank-csr.cpp
+++ examples/mpi-pagerank-csr.cpp
@@ -56,7 +56,7 @@ std::pair<size_t, std::map<uint_fast32_t, float>> pagerank(const Graph& graph)
         // gather the results of the matrix-vector products
         arma::fvec prod(n);
         MPI_Allgatherv(prod_block.memptr(), blocks_num_rows[rank], MPI_FLOAT,
-                    prod.memptr(), blocks_num_rows.data(), blocks_displacements.data(), MPI_FLOAT, MPI_COMM_WORLD);
+                       prod.memptr(), blocks_num_rows.data(), blocks_displacements.data(), MPI_FLOAT, MPI_COMM_WORLD);
 
         p = (1-d)/n * ones + d * (prod);
     }
